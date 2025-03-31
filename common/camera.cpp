53b89724ac9b8e9a5432d7d7345983985e5b6541
Michalowsk1 <23619282@stu.mmu.ac.uk>
2025-03-31 21:31:51 +0100

    basic startup

--- common/camera.cpp
+++ common/camera.cpp
@@ -1,1 +1,46 @@
+#include <common/camera.hpp>
 
+Camera::Camera(const vec3 Eye, const vec3 Target)
+{
+	eye = Eye;
+	target = Target;
+}
+
+void Camera::calculateMatrices()
+{
+	// Calculate camera vectors
+	calculateCameraVectors();
+
+	//CALCULATE view matrix
+	view = lookAt(eye, eye + front, worldUp);
+
+	//calculate projection matrix
+	projection = perspective(fov, aspect, near, far);
+}
+
+void Camera::calculateCameraVectors()
+{
+	front = glm::vec3(cos(yaw) * cos(pitch), sin(pitch), sin(yaw) * cos(pitch));
+	right = glm::normalize(glm::cross(front, worldUp));
+	up = glm::cross(right, front);
+}
+
+void Camera::quaternionCamera()
+{
+	// Calculate camera orientation quaternion from the Euler angles
+	Quaternion newOrientation(-pitch, yaw);
+
+	//Apply SLERP
+	orientation = Maths::SLERP(orientation, newOrientation, 0.2f);
+
+	// Calculate the view matrix
+	view = orientation.matrix() * Maths::translate(-eye);
+
+	// Calculate the projection matrix
+	projection = glm::perspective(fov, aspect, near, far);
+
+	// Calculate camera vectors from view matrix
+	right = glm::vec3(view[0][0], view[1][0], view[2][0]);
+	up = glm::vec3(view[0][1], view[1][1], view[2][1]);
+	front = -glm::vec3(view[0][2], view[1][2], view[2][2]);
+}
--- common/camera.hpp
+++ common/camera.hpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <common/maths.hpp>
+
+class Camera
+{
+public:
+	//projection parameters
+	float fov = Maths::radians(45.0f);
+	float aspect = 1024.0f / 768.0f;
+	float near = 0.2f;
+	float far = 100.0f;
+	vec3 right = vec3(1.0f, 0.0f, 0.0f);
+	vec3 up = vec3(0.0f, 1.0f, 0.0f);
+	vec3 front = vec3(0.0f, 0.0f, -1.0f);
+
+	// Camera Euler angles
+	float yaw = 0.0f;
+	float pitch = 0.0f;
+	float roll = 0.0f;
+
+	//camera vectors
+	vec3 eye;
+	vec3 target;
+	vec3 worldUp = vec3(0.0f, 1.0f, 0.0f);
+
+	//transformation matrices
+	mat4 view;
+	mat4 projection;
+
+	//Quaternion camera
+	Quaternion orientation = Quaternion(pitch, yaw);
+
+	//constructor
+	Camera(const vec3 eye, const vec3 target);
+
+	//methods
+	void calculateMatrices();
+
+	void calculateCameraVectors();
+
+	void quaternionCamera();
+};
